81090f9bae58960c2240c0952dfa029839a43f02
phplaber <phplaber@gmail.com>
2012-12-15 20:44:26 +0800

    添加单词例句的CURD操作业务逻辑

--- admin/process.php
+++ admin/process.php
@@ -3,8 +3,9 @@
 session_start();
 require('../mysql.class.php');
 require('../Smarty/Smarty.class.php');
+$db = require('../config.php');
+$pds = new MysqlConnect($db['server'], $db['user'], $db['password'], $db['db'], $db['charset']);
 $smarty = new Smarty;
-$pds = new MysqlConnect('localhost', 'root', '19880210', 'pds', 'utf8');
 
 // 查询所有单词的基本信息
 if ($_GET['action'] == 'basic')
@@ -18,26 +19,44 @@ if ($_GET['action'] == 'basic')
 	}
 	$smarty->assign("basics", $basic);
 	$smarty->display('basic.html');
-	
 }
-// 查询所有单词的基本例句
+// 查询所有单词的例句
 elseif ($_GET['action'] == 'example')
 {
-	$r = $pds->query("SELECT * FROM pds_example");
-	while ($result = $pds->fetchArray($r))
+	$r = $pds->query("SELECT * FROM pds_example ORDER BY wid ASC");
+	$count = $pds->getNumbers($r);
+	$example = array();
+	for ($i=0; $i < $count; $i++)
 	{
-		print_r($result);
+		$result = $pds->fetchArray($r);
+		// 用单词替换单词id
+		$word = $pds->fetchArray($pds->query("SELECT word FROM pds_basic WHERE id = " .$result['wid']));
+		$result['wid'] = $word['word'];
+		// 高亮显示例句中单词
+		$word['word'] = strtolower($word['word']);
+		$result['example_en'] = str_ireplace($word['word'], "<b style='color:green;'>{$word['word']}</b>", $result['example_en']);
+		$example[$i] = $result;
 	}
+	$smarty->assign("examples", $example);
+	$smarty->display('example.html');
 }
-// 展示“新增”表单
-elseif ($_GET['action'] == 'add')
+// 上传音频文件
+elseif($_GET['action'] == 'audio')
 {
-	$smarty->display('add.html');
+	// TODO
+	/*
+	1，展示所有音频文件（附带：音频文件的地址）
+	2，上传音频文件
+	*/
 }
-// 向数据库写入“新增”表单提交数据
-elseif ($_POST['anchor'] == 'add')
+// [begin 新增单词]
+elseif ($_GET['action'] == 'b_add')
 {
-	$word = $_POST['word'];
+	$smarty->display('b_add.html');
+}
+elseif ($_POST['anchor'] == 'b_add')
+{
+	$word = strtolower($_POST['word']);
 	$phonogram = $_POST['phonogram'];
 	$oxford = $_POST['oxford'];
 	$network = $_POST['network'];
@@ -45,34 +64,121 @@ elseif ($_POST['anchor'] == 'add')
 	$r = $pds->query("INSERT INTO pds_basic VALUES(null, '" .$word. "', '" .$phonogram. "', '" .$oxford. "', '" .$network. "')");
 	if ($r) echo '新增成功';
 }
+// [end 新增单词]
+
 // 删除单词
-elseif ($_GET['action'] == 'delete')
+elseif ($_GET['action'] == 'b_delete')
 {
 	$id = $_GET['id'];
 	$r = $pds->query("DELETE FROM pds_basic WHERE id = " .$id );
 	if ($r) echo '<p style="font-size: 13px; font-weight: bold; color: green;">删除成功</p>';
 }
-// 编辑单词
-elseif ($_GET['action'] == 'edit')
+
+// [begin 更新单词]
+elseif ($_GET['action'] == 'b_edit')
 {
 	$id = $_GET['id'];
 	$r = $pds->query("SELECT * FROM pds_basic WHERE id = " .$id );
 	$basic = $pds->fetchArray($r);
-	//print_r($basic);exit;
 	$smarty->assign("basic", $basic);
-	$smarty->display('edit.html');
+	$smarty->display('b_edit.html');
 }
-elseif ($_POST['anchor'] == 'edit')
+elseif ($_POST['anchor'] == 'b_edit')
 {
 	$id = $_POST['id'];
-	$word = $_POST['word'];
 	$phonogram = $_POST['phonogram'];
 	$oxford = $_POST['oxford'];
 	$network = $_POST['network'];
 	
-	$r = $pds->query("UPDATE pds_basic SET word = '" .$word. "', phonogram = '" .$phonogram. "', oxford_def = '" .$oxford. "', network_def = '" .$network. "' WHERE id = {$id}");
+	$r = $pds->query("UPDATE pds_basic SET phonogram = '" .$phonogram. "', oxford_def = '" .$oxford. "', network_def = '" .$network. "' WHERE id = {$id}");
 	if ($r) echo '更新成功';
 }
+// [end 更新单词]
+
+// 删除单词例句
+elseif ($_GET['action'] == 'e_delete')
+{
+	$id = $_GET['id'];
+	$r = $pds->query("DELETE FROM pds_example WHERE id = " .$id );
+	if ($r) echo '<p style="font-size: 13px; font-weight: bold; color: green;">删除成功</p>';
+}
+
+// [begin 新增单词例句]
+elseif ($_GET['action'] == 'e_add')
+{
+	$smarty->display('e_add.html');
+}
+elseif ($_POST['anchor'] == 'e_add')
+{
+	$word = $_POST['word'];
+	$example_en_1 = $_POST['example_en_1'];
+	$example_zh_1 = $_POST['example_zh_1'];
+
+	// 通过$word在pds_basic中找到对应id
+	$tmp1 = $pds->query("SELECT id FROM pds_basic WHERE word = '" .$word. "' LIMIT 1");
+	$tmp2 = $pds->fetchArray($tmp1);
+	//var_dump($tmp2);exit;
+	if (!$tmp2)
+	{
+		echo "单词{$word}不存在，请先新增单词。";
+		exit;
+	}
+	$wid = $tmp2['id'];
+
+	// 当example_en_2为空时，正常情况下example_en_3也应该为空。
+	if ($_POST['example_en_2'] == '')
+	{
+		$r = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_1. "', '" .$example_zh_1. "')");
+		if ($r)	echo '新增成功';
+	}
+	else
+	{
+		$example_en_2 = $_POST['example_en_2'];
+		$example_zh_2 = $_POST['example_zh_2'];
+		if ($_POST['example_en_3'] == '')
+		{
+			$r1 = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_1. "', '" .$example_zh_1. "')");
+			$r2 = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_2. "', '" .$example_zh_2. "')");
+			if ($r1 && $r2)	echo '新增成功';
+		}
+		else
+		{
+			$example_en_3 = $_POST['example_en_3'];
+			$example_zh_3 = $_POST['example_zh_3'];
+
+			$r1 = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_1. "', '" .$example_zh_1. "')");
+			$r2 = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_2. "', '" .$example_zh_2. "')");
+			$r3 = $pds->query("INSERT INTO pds_example VALUES(null, '" .$wid. "', '" .$example_en_3. "', '" .$example_zh_3. "')");
+			if ($r1 && $r2 && $r3)	echo '新增成功';
+		}
+	}
+
+}
+// [end 新增单词例句]
+
+// [begin 更新单词例句]
+elseif ($_GET['action'] == 'e_edit')
+{
+	$id = $_GET['id'];
+	$r = $pds->query("SELECT * FROM pds_example WHERE id = " .$id );
+	$example = $pds->fetchArray($r);
+	$tmp1 = $pds->query("SELECT word FROM pds_basic WHERE id =". $example['wid']);
+	$tmp2 = $pds->fetchArray($tmp1);
+	$example['word'] = $tmp2['word'];
+	$smarty->assign("example", $example);
+	$smarty->display('e_edit.html');
+}
+elseif ($_POST['anchor'] == 'e_edit')
+{
+	$id = $_POST['id'];
+	$example_en = $_POST['example_en'];
+	$example_zh = $_POST['example_zh'];
+
+	$r = $pds->query("UPDATE pds_example SET example_en = '" .$example_en. "', example_zh = '" .$example_zh. "' WHERE id = {$id}");
+	if ($r) echo '更新成功';
+}
+// [end 更新单词例句]
+
 // 注销退出
 elseif ($_GET['action'] == 'exit')
 {
